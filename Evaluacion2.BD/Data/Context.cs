caec8ff2399986f94cac658b7f2c46a4f3a72aec
Fiorella Rizzo <Rizzofiorella@outlook.com>
2024-08-14 02:56:29 -0300

    Agregar archivos de proyecto.

---
+++ Evaluacion2.BD/Data/Context.cs
@@ -0,0 +1,26 @@
+﻿using Evaluacion2.BD.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluacion2.BD.Data
+{
+    public class Context : DbContext
+    //esta clase representara en mi programa la base de datos
+    {
+        //el metodo DbSet es una clase generica que incorpora las tablas de la BD
+        public DbSet<TDocumento> TDocumentos { get; set; }
+        public DbSet<Persona> Personas { get; set; }
+
+
+
+        //genero el constructor con opciones
+        public Context(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
---
+++ Evaluacion2.Server/Program.cs
@@ -0,0 +1,33 @@
+using Evaluacion2.BD.Data;
+using Microsoft.EntityFrameworkCore;
+
+//configuracion de los servicios en el constructor de la aplicacion
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<Context>(op => op.UseSqlServer("name=Conn"));  
+
+
+//construccion de la aplicacion
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
